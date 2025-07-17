09606011cc12fe463d41dd7ff2d394ab32aa210b
Suryavanshi11444 <modi00447@gmail.com>
2025-07-17 15:26:27 +0530

    save all changes

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,17 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema({
-
-// name: String,
-
-// email: { type: String, unique: true },
-
-// password: String,
-
-// });
-
-// module.exports = mongoose.model("User", userSchema);
-
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,3 @@
-// const express = require("express");
-
-// const mongoose = require("mongoose");
-
-// const dotenv = require("dotenv");
-
-// const cors = require("cors");
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-
-// app.use(express.json());
-
-// mongoose.connect(process.env.MONGO_URI,
-
-// { useNewUrlParser: true, useUnifiedTopology: true })
-
-// .then(() => console.log("MongoDB Connected"))
-
-// .catch(err => console.log(err));
-
-// app.use("/api/auth", require("./routes/auth"));
-
-// app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`));
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
